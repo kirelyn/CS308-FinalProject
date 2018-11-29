6627b8a6445a87c669eb68465e52ddeb1ee2831c
Kennedy Griffin <kennedyg@viper.cs.ksu.edu>
2018-11-29 15:28:26 -0600

    Main file added and split

---
+++ pacman.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct pacman {
+	int startx;
+	int starty;
+}PAC;
---
+++ player.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+# include <stdio.h> 
+#include <unistd.h> /* for sleep() */
+#include <curses.h>
+
+
+typedef struct Player{
+	char * name;
+	int score;
+} PLAYER;
